07e9a03aaad7478e270c52ea953011c22064f9c6
Hung Nguyen <nd.hung@me.com>
2025-07-07 16:22:51 +0200

    refactor: Remove unused image and adjust video brightness in Hero component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,30 +6,18 @@ import Link from "next/link"
 export default function Hero() {
 	return (
 		<>
-			<section className="relative w-full h-svh overflow-hidden flex items-start justify-center bg-black">
+			<section className="relative w-full h-svh overflow-hidden flex items-start justify-center ">
 				{/*
 				 Background Video */}
-				<Image
-					src="/images/e1.jpg"
-					alt="Address Hotels + Resorts"
-					width={200}
-					height={100}
-					className="absolute inset-0 w-full h-full opacity-90 object-cover object-center   z-0"
-				/>
-				{/* 
-				<video
-					autoPlay
-					muted
-					loop
-					playsInline
-					className="absolute inset-0 w-full h-full opacity-70 object-cover object-center  brightness-60 z-0">
+
+				<video autoPlay muted loop playsInline className="absolute inset-0 w-full h-full object-cover object-center  brightness-80 z-0">
 					<source
 						src="https://www-addresshotels-com.azureedge.net/wp-content/uploads/2024/11/Address-Hotels-Landscape-Video.mp4"
 						type="video/mp4"
 					/>
 				</video>
 
-				Overlay Content */}
+				{/* Overlay Content */}
 
 				<div className="relative z-10 text-white text-center px-4 md:px-8 lg:px-16 xl:px-32">
 					<div className="my-6 flex justify-center">
